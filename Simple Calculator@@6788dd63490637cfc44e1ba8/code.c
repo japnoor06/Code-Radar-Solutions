aa184b633f654c812b10cc024a7a0fc61221d34e
Japnoor Kaur <japnoor1107.becse24@chitkara.edu.in>
2025-02-04 00:52:16 +0530

    Solution to Simple Calculator

---
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main(){
+    int a,b,result;
+    char operator;
+    scanf("%d%d",&a,&b);
+    scanf("%c",&operator);
+    if(operator=='+'){
+      result=a+b;
+      printf("%d",result);
+    }
+    else if(operator=='-'){
+        result=a-b;
+        printf("%d",result);
+    }
+    else if(operator=='*'){
+        result=a*b;
+        printf("&d",result);
+    }
+    else if(operator=='/'){
+        if(b!=0){
+            result=a/b;
+            printf("%d",result);
+        }
+        else{
+            printf("Not Divisible");
+        }
+    }
+    else{
+        printf("Invalid");
+    }
+}
